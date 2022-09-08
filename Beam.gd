11f6c17159b0654fa758050a5396d5c0d91d9c77
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-09-08 11:17:41 +0900

    9/5

--- Beam.gd
+++ Beam.gd
@@ -15,27 +15,34 @@ func _physics_process(delta: float) -> void:
 	var cast_point := cast_to
 	force_raycast_update()
 	
+	$CollisionParticles.emitting = is_colliding()
+	
 	if is_colliding():
 		cast_point = to_local(get_collision_point())
+		$CollisionParticles.global_rotation - get_collision_normal().angle()
+		$CollisionParticles.position = cast_point
 		
 	$Line2D.points[1] = cast_point
+	$BeamParticles.position = cast_point * 0.5
+	$BeamParticles.process_material.emission_box_extents.x = cast_point.length() * 0.5
 
 func set_is_casting(cast : bool) -> void:
 	is_casting = cast
 	
+	$BeamParticles.emitting = is_casting
+	$CastingParticles.emitting = is_casting
 	if is_casting:
 		appear()
 	else:
+		$CollisionParticles.emitting = false
 		disappear()
 
 	set_physics_process(is_casting)
 	
 func appear() -> void:
-	$Line2D.width = 0
-	var tween := create_tween()
-	tween.tween_property($Line2D,"width", 10.0, 0.2)
+	var tween1 := create_tween()
+	tween1.tween_property($Line2D,"width", 10.0, 0.2)
 	
 func disappear() -> void:
-	$Line2D.width = 10
-	var tween := create_tween()
-	tween.tween_property($Line2D,"width", 0, 0.1)
+	var tween2 := create_tween()
+	tween2.tween_property($Line2D,"width", 0.0, 0.1)
